0dc5e7bad476ec8e371024880d008d2ee4341db9
Abb6781 <151780295+Abb6781@users.noreply.github.com>
2025-11-07 01:11:20 -0800

    whale and krill movement

--- Assets/Scripts/KrillScript.cs
+++ Assets/Scripts/KrillScript.cs
@@ -1,31 +1,20 @@
 using UnityEngine;
 
-public class whaleScript : MonoBehaviour
+public class KrillScript : MonoBehaviour
 {
-    public GameObject krillPrefab;
-    public float spawnInterval = 2f;
-    private float timeSinceLastSpawn;
     public float movementSpeed = 2f;
     public Vector2 pointA;
     public Vector2 pointB;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        timeSinceLastSpawn = spawnInterval;
-
+        pointA = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        timeSinceLastSpawn += Time.deltaTime;
-        if(timeSinceLastSpawn >= spawnInterval){
-            Instantiate(krillPrefab,transform.position,Quaternion.identity);
-            timeSinceLastSpawn = 0f;
-        }
-
         float t = Mathf.PingPong(Time.time * movementSpeed, 1);
-
         transform.position = Vector2.Lerp(pointA, pointB, t);
     }
 }
