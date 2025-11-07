971507e6415119b74f9e5d7cdf3f956a01f9da74
Abb6781 <151780295+Abb6781@users.noreply.github.com>
2025-11-06 22:32:54 -0800

    anglerfish animation

--- Assets/Scripts/anglerfishScript.cs
+++ Assets/Scripts/anglerfishScript.cs
@@ -3,10 +3,15 @@ using UnityEngine.SceneManagement;
 public class anglerfishScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public SpriteRenderer spriteRenderer;
+    public Sprite attackSprite; 
     void OnCollisionEnter2D(Collision2D collision){
         if(collision.gameObject.name == "box" || collision.gameObject.name == "Squid"){
             //animation here
+            spriteRenderer.sprite = attackSprite;
+            transform.localScale = new Vector3(1.5f, 1.5f, 1f);
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            
         }
     }
     void Start()
