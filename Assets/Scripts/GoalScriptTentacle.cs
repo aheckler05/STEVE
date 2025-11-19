1e1ff89a8c05c1ca34a2db69f00621b8212f05e0
Abb6781 <151780295+Abb6781@users.noreply.github.com>
2025-11-18 22:59:02 -0800

    tentacle puzzle first sample

---
+++ Assets/Scripts/GoalScriptTentacle.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class GoalScriptTentacle : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+    void OnMouseDown(){
+        Debug.Log("Success!");
+        //next puzzle
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
