7865d5b6d4619fab2151936c77a78a386883008b
Abb6781 <151780295+Abb6781@users.noreply.github.com>
2025-11-18 23:27:37 -0800

    urchins working

--- Assets/Scripts/TentacleScript.cs
+++ Assets/Scripts/TentacleScript.cs
@@ -6,13 +6,15 @@ public class NewMonoBehaviourScript : MonoBehaviour
     private Vector3 offset;
     private bool isDragging = false;
     private Rigidbody2D rb;
+    private bool alive = true;
+    private SpriteRenderer spriteRenderer;
     
     void OnMouseDown(){
         offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
         isDragging = true;
     }
     void OnMouseDrag(){
-        if(isDragging){
+        if(isDragging && alive){
             Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             //transform.position = new Vector3(mouseWorldPosition.x + offset.x, mouseWorldPosition.y+offset.y,transform.position.z);
             rb.MovePosition(mouseWorldPosition);
@@ -24,11 +26,20 @@ public class NewMonoBehaviourScript : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
-
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.name == "Urchin"){
+            Debug.Log("Collision!");
+            rb.constraints = RigidbodyConstraints2D.FreezePositionY;
+            rb.constraints = RigidbodyConstraints2D.FreezePositionX;
+            alive = false;
+            spriteRenderer.color = new Color(0.5f,0.5f,0.5f,1f);
+        }
     }
 }
