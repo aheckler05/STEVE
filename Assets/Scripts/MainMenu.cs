e6e3ed245ecfd8a746f711c712dcd2531c721a2d
Valerie <aheckler@unr.edu>
2025-11-20 01:03:23 -0800

    audio interface + restart button
    egg

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+
 
 public class MainMenu : MonoBehaviour
 {
+
+    AudioManager audioManager;
+    
+    private void Awake()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+    }
     public void play()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(playButtonSFX());
     }
     
+    public void settingBtn()
+    {
+        audioManager.PlaySFX(audioManager.button);
+    }
     public void quitGame()
     {
         Debug.Log("quitting");
+        audioManager.PlaySFX(audioManager.button);
         Application.Quit();
+    }
+
+    IEnumerator playButtonSFX()
+    {
+        audioManager.PlaySFX(audioManager.button);
+        yield return new WaitForSecondsRealtime(audioManager.button.length);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        
     }
 }
--- Assets/Scripts/SelectLevelButtons.cs
+++ Assets/Scripts/SelectLevelButtons.cs
@@ -1,17 +1,26 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class SelectLevelButtons : MonoBehaviour
 {
+    AudioManager audioManager;
+    private void Awake()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
      public void level1()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(button1SFX());
     }
-
-    // Update is called once per frame
-    void Update()
+    IEnumerator button1SFX()
     {
+        audioManager.PlaySFX(audioManager.button);
+        yield return new WaitForSecondsRealtime(audioManager.button.length);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         
     }
 }
