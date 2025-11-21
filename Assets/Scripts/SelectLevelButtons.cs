18a1a379052f00f9b2bb6feefd42547bde0a5b37
Abb6781 <151780295+Abb6781@users.noreply.github.com>
2025-11-21 03:40:21 -0800

    merge reversed

--- Assets/Scripts/SelectLevelButtons.cs
+++ Assets/Scripts/SelectLevelButtons.cs
@@ -1,19 +1,31 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class SelectLevelButtons : MonoBehaviour
 {
+    AudioManager audioManager;
+    private void Awake()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
      public void level1()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(button1SFX());
     }
     public void level2(){
         SceneManager.LoadScene(5);
     }
     // Update is called once per frame
-    void Update()
+    void Update(){
+    IEnumerator button1SFX()
     {
+        audioManager.PlaySFX(audioManager.button);
+        yield return new WaitForSecondsRealtime(audioManager.button.length);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         
-    }
+    }}
 }
