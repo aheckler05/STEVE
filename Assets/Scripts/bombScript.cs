a2e07282c3e244a5115ec14ef94ab642266ae2c3
Valerie <aheckler@unr.edu>
2025-12-05 00:44:29 -0800

    wee particle fx and wall retex+

--- Assets/Scripts/bombScript.cs
+++ Assets/Scripts/bombScript.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class bombScript : MonoBehaviour
 {
-
+    public GameObject explode;
+    public GameObject blood;
+    public GameObject death;
     AudioManager audioManager;
     bossController boss;
     private void Awake()
@@ -13,20 +15,25 @@ public class bombScript : MonoBehaviour
     }
     void OnCollisionEnter2D(Collision2D collision){
         if(collision.gameObject.name.Contains("sandBottomLevel")){
+            Instantiate(explode, transform.position, Quaternion.identity);
             audioManager.PlaySFX(audioManager.explode);
             Destroy(this.gameObject);
         }
         if(collision.gameObject.name.Contains("shark(Clone)")){
+            Instantiate(explode, transform.position, Quaternion.identity);
+            Instantiate(blood, collision.gameObject.transform.position, Quaternion.identity);
             audioManager.PlaySFX(audioManager.explode);
             Destroy(this.gameObject);
             Destroy(collision.gameObject);
         }
         if(collision.gameObject.name.Contains("bomb(Clone)")){
+            Instantiate(explode, transform.position, Quaternion.identity);
             audioManager.PlaySFX(audioManager.explode);
             Destroy(this.gameObject);
             Destroy(collision.gameObject);
         }
         if(collision.gameObject.name.Contains("Hook")){
+            Instantiate(explode, transform.position, Quaternion.identity);
             audioManager.PlaySFX(audioManager.explode);
             Destroy(this.gameObject);
             Destroy(collision.gameObject);
